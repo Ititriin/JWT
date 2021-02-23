3a2c65b0a21369c613c04634d8eb9afde44707e1
Ititriin <triin.tajur@vikk.ee>
2021-02-23 15:36:47 +0200

    add util folder

---
+++ src/util/index.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+const setTokenToCookie = (response, payload) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+    });
+    response.cookie('jwt_token', token, { maxAge: process.env.JWT_COOKE_MAX_AGE, httpOnly: true });
+};
+
+module.exports = {
+    setTokenToCookie,
+};
