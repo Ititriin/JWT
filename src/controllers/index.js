b0fc96c71462e91c570f2d863ffe5986b65373ac
Triin <73930703+Ititriin@users.noreply.github.com>
2021-02-25 17:02:20 +0200

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const main = require('./main');
 const login = require('./login');
 const register = require('./register');
+const dashboard = require('./dashboard');
 
 module.exports = {
     ...main,
     ...login,
     ...register,
+    ...dashboard,
 };
