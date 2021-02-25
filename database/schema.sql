8f185cb7c15403282a42fe58cec439b819fe59e0
Triin <73930703+Ititriin@users.noreply.github.com>
2021-02-25 17:13:42 +0200

    this is not necessary

--- database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id integer NOT NULL PRIMARY KEY,
-    username text NOT NULL UNIQUE, -- unikaalsed väärtused
-    password text NOT NULL,
-    email text NOT NULL UNIQUE,
-    first_name text NOT NULL,
-    last_name text NOT NULL
-);
