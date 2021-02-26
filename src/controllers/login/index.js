c99e23453ad8963cdce02db73162d5e29521adda
Ititriin <triin.tajur@vikk.ee>
2021-02-26 10:02:23 +0200

    enable correct login and login checks

--- src/controllers/login/index.js
+++ src/controllers/login/index.js
@@ -1,28 +1,29 @@
 const User = require('../../models/user.model');
+const username = require('../../models/user.model');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const renderLoginPage = (req, res) => {
-  const { email, message } = req.query;
-  res.render('login', { email, message });
+  const { username, message } = req.query;
+  console.log(req.query);
+  res.render('login', { username, message });
 };
 
 const userLogin = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
-  const user = await User.findOne({ email }).lean();
+  const user = await User.findOne({ username }).lean();
 
   if (!user) {
-    return res.redirect(`/login?message=${encodeURIComponent('Invalid email!')}&email=${encodeURIComponent(email)}`);
+    return res.redirect(`/login?message=${encodeURIComponent('Invalid username!')}&username=${encodeURIComponent(username)}`);
   }
 
   if (bcryptjs.compareSync(password, user.password)) {
-    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
     res.cookie('jwt_token', token, { httpOnly: true });
     return res.redirect('/dashboard');
   }
-
-  res.redirect(`/login?message=${encodeURIComponent('Invalid password!')}&email=${encodeURIComponent(email)}`);
+  res.redirect(`/login?message=${encodeURIComponent('Invalid password!')}&password=${encodeURIComponent(username)}`);
 };
 
 module.exports = {
